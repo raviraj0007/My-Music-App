1d8550496546447238b6fc65114ea7ff716ffd56
raviraj0007 <raviraj04066@gmail.com>
2024-08-16 21:02:54 +0530

    json_data_display

--- app/src/main/java/com/example/mymusicapp/MainActivity.kt
+++ app/src/main/java/com/example/mymusicapp/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,12 +17,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myRecycleView: RecyclerView
+    lateinit var myAdapter: MyAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-
+        myRecycleView= findViewById(R.id.recyclerView)
         val retrofitBuilder = Retrofit.Builder()
             .baseUrl("https://deezerdevs-deezer.p.rapidapi.com/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -30,10 +35,16 @@ class MainActivity : AppCompatActivity() {
 
 
         retrofitData.enqueue(object : Callback<MyData> {
+
             override fun onResponse(p0: Call<MyData?>, p1: Response<MyData?>) {
-                val datalist= p1.body()?.data
-                val textView= findViewById<TextView>(R.id.hellotext)
-                textView.text= datalist.toString()
+                val datalist= p1.body()?.data!!
+            //    val textView= findViewById<TextView>(R.id.hellotext)
+            //    textView.text= datalist.toString()\
+
+                myAdapter= MyAdapter(this@MainActivity,datalist)
+                myRecycleView.adapter= myAdapter
+                myRecycleView.layoutManager= LinearLayoutManager(this@MainActivity)
+
                 Log.d("TAG: onResponse", "onResponse: " + p1.body())
             }
 
